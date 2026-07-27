69ac62fbcd9cc2059e0219f84dea62733500c6a4
Ömer Faruk Ölünk <omrfrk72004@gmail.com>
2026-07-27 08:40:56 +0300

    refactor(repository): add missing @Repository annotation to UserRepository for architectural consistency

--- src/main/java/com/educonnect/repository/UserRepository.java
+++ src/main/java/com/educonnect/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.educonnect.repository;
 
 import com.educonnect.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // Kullanıcı adına göre veritabanında arama yapar (Giriş yaparken hayati önem taşır)
